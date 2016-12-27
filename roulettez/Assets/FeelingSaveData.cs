b27b7057490207020af1912e53bcd1b490ae10f5
sasumaki <sasu.makinen@helsinki.fi>
2016-12-27 17:09:28 +0200

    kapuloita rattaisiin t.SaveData

---
+++ roulettez/Assets/FeelingSaveData.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class FeelingSaveData : MonoBehaviour {
+
+    public SaveData saveData;
+
+    [Serialized] public DateTime date;
+
+    [Serialized] public string feeling;
+
+
+	// Use this for initialization
+	void Start () {
+
+        date = DateTime.Now;
+
+	}
+
+    // Update is called once per frame
+    void Save()
+    {
+        saveData.Save();
+    }
+    
+    void Load()
+    {
+
+    }
+}
+
+internal class SerializedAttribute : Attribute
+{
+}
---
+++ roulettez/Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneChanger : MonoBehaviour
+{
+
+    public void LoadScene(int sceneNumber)
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(1);
+    }
+}
